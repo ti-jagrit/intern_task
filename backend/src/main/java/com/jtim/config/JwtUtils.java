2687384dca30508c991c753102e2516a99b7bb96
ti-jagrit <ti.jagrit@gmail.com>
2025-07-12 19:31:24 +0545

    backend project create and git init

---
+++ backend/src/main/java/com/jtim/config/JwtUtils.java
@@ -0,0 +1,82 @@
+package com.jtim.config;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class JwtUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);;
+	@Value("${spring.app.jwtSecret}")
+	private String jwtSecret;
+	
+	@Value("${spring.app.jwtExpirationMs}")
+	private int jwtExpirationMs;
+	
+	public String getJwtToeknFromHeader(HttpServletRequest request) {
+		String token=request.getHeader("Authorization");;
+		logger.debug("Authorization Header: {}",token);
+		if(token!=null && token.startsWith("Bearer ")) {
+			return token.substring(7);
+		}
+		return null;
+	}
+	
+	public String generateTokenFromUserName(UserDetails userDetails) {
+		String username=userDetails.getUsername();
+		return Jwts.builder().setSubject(username).setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime()+jwtExpirationMs))
+				.signWith(key()).compact();
+	}
+	
+	public String getUserNameFromToken(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(key())
+				.build()
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
+	
+	private Key key() {
+		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+	}
+	
+	public boolean validateJwtToken(String token) {
+		try {
+			Jwts.parserBuilder()
+			.setSigningKey(key())
+			.build()
+			.parseClaimsJws(token);
+			return true;
+			
+		} catch (MalformedJwtException e) {
+			logger.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			logger.error("JWT token is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			logger.error("JWT token is unsupported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			logger.error("JWT claims string is empty: {}", e.getMessage());
+		}
+		return false;
+	}
+
+	
+	
+	
+}
---
+++ backend/src/test/java/com/jtim/InternTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
